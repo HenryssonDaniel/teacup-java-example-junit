d96b685ac69fd4a2543797024d644f4518d92cf8
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-08-22 17:35:26 -0500

    Use FTP reply builder
    The FTP test should use the reply builder to build the reply, rather
    than a custom implementation.

--- src/test/java/io/github/henryssondaniel/teacup/examples/junit/ftp/SimpleTest.java
+++ src/test/java/io/github/henryssondaniel/teacup/examples/junit/ftp/SimpleTest.java
@@ -6,35 +6,26 @@ import io.github.henryssondaniel.teacup.protocol.ftp.Client;
 import io.github.henryssondaniel.teacup.protocol.ftp.SimpleServer;
 import io.github.henryssondaniel.teacup.protocol.ftp.client.Command;
 import io.github.henryssondaniel.teacup.protocol.ftp.server.Factory;
-import io.github.henryssondaniel.teacup.protocol.ftp.server.Reply;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 @Fixture(SimpleSetup.class)
 class SimpleTest {
+  private static final int CODE = 200;
   private static final int PORT = 1234;
   private static final SimpleServer SIMPLE_SERVER =
       Teacup.getServer(SimpleServer.class, Constants.FTP_SERVER);
 
   @Test
   void sendHttpRequest() throws IOException {
-    var requests = SIMPLE_SERVER.setContext(Factory.createContextBuilder(new TestReply()).build());
+    var requests =
+        SIMPLE_SERVER.setContext(
+            Factory.createContextBuilder(Factory.createReplyBuilder(CODE, "message").build())
+                .build());
     var client = Teacup.getClient(Client.class, Constants.FTP_CLIENT);
     client.connect("localhost", PORT);
     client.send(Command.ACCT, "argument");
     requests.get();
     SIMPLE_SERVER.removeSupplier(requests);
-  }
-
-  private static class TestReply implements Reply {
-    @Override
-    public int getCode() {
-      return 200;
-    }
-
-    @Override
-    public String getMessage() {
-      return "message";
-    }
   }
 }
