992be49a5f929a11a84a48fe9d3704748894f7dd
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-08-28 13:32:15 -0500

    Add verification for FTP test
    Added verification for the FTP test.

--- src/test/java/io/github/henryssondaniel/teacup/examples/junit/ftp/SimpleTest.java
+++ src/test/java/io/github/henryssondaniel/teacup/examples/junit/ftp/SimpleTest.java
@@ -1,31 +1,72 @@
 package io.github.henryssondaniel.teacup.examples.junit.ftp;
 
 import io.github.henryssondaniel.teacup.core.Fixture;
+import io.github.henryssondaniel.teacup.core.Node;
 import io.github.henryssondaniel.teacup.engine.junit.Teacup;
 import io.github.henryssondaniel.teacup.protocol.ftp.Client;
 import io.github.henryssondaniel.teacup.protocol.ftp.SimpleServer;
 import io.github.henryssondaniel.teacup.protocol.ftp.client.Command;
+import io.github.henryssondaniel.teacup.protocol.ftp.client.Response;
 import io.github.henryssondaniel.teacup.protocol.ftp.server.Factory;
+import io.github.henryssondaniel.teacup.protocol.ftp.server.Request;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 @Fixture(SimpleSetup.class)
 class SimpleTest {
+  private static final String ARGUMENT = "argument";
   private static final int CODE = 200;
+  private static final String MESSAGE = "message";
   private static final int PORT = 1234;
-  private static final SimpleServer SIMPLE_SERVER =
-      Teacup.getServer(SimpleServer.class, Constants.FTP_SERVER);
 
   @Test
   void sendHttpRequest() throws IOException {
+    var simpleServer = Teacup.getServer(SimpleServer.class, Constants.FTP_SERVER);
+
     var requests =
-        SIMPLE_SERVER.setContext(
-            Factory.createContextBuilder(Factory.createReplyBuilder(CODE, "message").build())
+        simpleServer.setContext(
+            Factory.createContextBuilder(Factory.createReplyBuilder(CODE, MESSAGE).build())
                 .build());
+
     var client = Teacup.getClient(Client.class, Constants.FTP_CLIENT);
     client.connect("localhost", PORT);
-    client.send(Command.ACCT, "argument");
-    requests.get();
-    SIMPLE_SERVER.removeSupplier(requests);
+
+    createResponse().verify(client.send(Command.ACCT, ARGUMENT));
+    verifyRequests(requests.get());
+
+    simpleServer.removeSupplier(requests);
+  }
+
+  private static Node<Response> createResponse() {
+    return io.github.henryssondaniel.teacup.protocol.ftp.node.Factory.createResponseBuilder()
+        .setCode(
+            io.github.henryssondaniel.teacup.core.assertion.Factory.createIntegerAssert()
+                .isEqualTo(CODE))
+        .setText(
+            io.github.henryssondaniel.teacup.core.assertion.Factory.createStringAssert()
+                .isEqualTo(CODE + " " + MESSAGE + System.lineSeparator()))
+        .build();
+  }
+
+  private static void verifyRequests(List<? extends Request> requests) {
+    io.github.henryssondaniel.teacup.core.assertion.Factory.createListAssert()
+        .hasSize(1)
+        .verify(requests);
+
+    io.github.henryssondaniel.teacup.protocol.ftp.node.Factory.createRequestBuilder()
+        .setArgument(
+            io.github.henryssondaniel.teacup.core.assertion.Factory.createStringAssert()
+                .isEqualTo(ARGUMENT))
+        .setCommand(
+            io.github.henryssondaniel.teacup.core.assertion.Factory.createStringAssert()
+                .isEqualTo("ACCT"))
+        .setReceivedTime(
+            io.github.henryssondaniel.teacup.core.assertion.Factory.createLongAssert()
+                .isLessThanOrEqualTo(Date.from(Instant.now()).getTime()))
+        .build()
+        .verify(requests.get(0));
   }
 }
